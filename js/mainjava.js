fdf4fb40abc61ccd8b91d673f065c01f3d835cc1
Jonas <jona345e@edu.sde.dk>
2019-04-08 13:04:11 +0200

    første

---
+++ js/mainjava.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ $( "#main" ).load( "port.html" ); })
+
+function loadinfo(){
+  $( "#main" ).load( "info.html" );
+}
+
+function loadport(){
+  $( "#main" ).load( "port.html" );
+}
